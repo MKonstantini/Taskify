11141a12bf0ec503398e72dea609bf4ae1055955
Matan Konstantini <matanko35@gmail.com>
2024-05-27 12:57:03 +0300

    updates

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import { Medal } from 'lucide-react'
+import Link from 'next/link'
 
 const MarketingPage = () => {
     return (
         <div className='flex items-center justify-center flex-col'>
-            <div className='flex items-center justify-center flex-col'>
+            <header className='flex items-center justify-center flex-col'>
                 <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
                     <Medal className='h-6 w-6 me-2'></Medal>
                     <div>Marketing Page</div>
@@ -12,10 +14,13 @@ const MarketingPage = () => {
                 <div className='text-3xl md:tex-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit '>
                     work forward.
                 </div>
-            </div>
-            <div>
+            </header>
+            <section>
                 <p className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is uniue - accomplish it all with Taskify.</p>
-            </div>
+                <Button className='mt-6' size="lg" asChild>
+                    <Link href={'/sign-up'}>Get Taskify for free</Link>
+                </Button>
+            </section>
         </div>
     )
 }
